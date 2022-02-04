2b406dd6fe695b89c5ffddd113d64b4d546f3ef2
satorina-vDev <nonato.miau@hotmail.com>
2022-02-04 02:54:09 -0300

    trabalho3

---
+++ script.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  
+  //2
+  
+  casa = 11;
+  
+  while (casa > 1) {
+    casa = casa - 1;
+    console.log(casa);
+  }
+  
+  //3
+  
+  for (let o = 1; o <= 100; o += 2) {
+    console.log(o);
+  }
+  
+  //4
+  
+  for (let p = 0; p <= 100; p += 2) {
+    console.log(p);
+  }
+  
+  //5
+  
